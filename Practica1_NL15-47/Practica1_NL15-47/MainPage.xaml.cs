d288c9f63d10419b2778b5dec07eea24e0a0292e
figuedelmen <eliasfigue2019@gmail.com>
2026-03-17 12:50:57 -0600

    Proyecto limpio

---
+++ Practica1_NL15-47/Practica1_NL15-47/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Practica1_NL15_47
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSumar_Clicked(object sender, EventArgs e)
+        {
+            var numero1 = Convert.ToInt32(TexNumero1.Text);
+
+            var numero2 = Convert.ToInt32(TexNumero2.Text);
+
+            var suma = numero1 + numero2;
+
+            TexResultado.Text = suma.ToString();
+        }
+    }
+}
